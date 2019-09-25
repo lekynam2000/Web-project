66cc7247d552a5a2e44442d6f1fbf9ce99c3a45b
lekynam2000 <53005356+lekynam2000@users.noreply.github.com>
2019-09-25 20:53:11 +0800

    Add files via upload

---
+++ decoderOOP.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 11:23:44 2019
+
+@author: DELL
+"""
+from code_tabel import keyList
+import numpy as np
+from PIL import Image
+class decoder(object):
+    def __init__(self):
+        pass
+        
+    def decode(self,address,key):
+        c = 0
+        im=Image.open(address)
+        na=np.array(im).flatten()
+        print(len(na))
+        textList=[]
+        for x in na:
+           try: 
+               textList.append(chr(keyList[key%100].index(x)))
+               
+           except:
+               textList.append('')
+               print("Error")
+               c += 1
+        with open('your_file.txt', 'w') as f:    
+            for item in textList:
+                f.write(item)       
+address="result.png"
+key=12
+whatever=decoder()
+whatever.decode(address,key)
---
+++ encoderOOP.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 21:57:38 2019
+
+@author: DELL
+"""
+from code_tabel import keyList
+import numpy as np
+import math as m
+from PIL import Image
+class encoder(object):
+    def __init__(self,file):
+        self.file=file        
+    def encode(self,key):
+        lineList = [x for y in open(self.file) for x in y]
+        ln=len(lineList)
+        for i in range(3*(1+ln//3)-ln):
+            lineList.append(" ");
+        colorList = []
+        for x in lineList:
+            colorList.append(keyList[key%100][ord(x)])
+        print(len(colorList)/3)
+        length=int(len(colorList)/3)
+        n=int(m.sqrt(length))+1
+        for i in range(n**2-length):
+            for _ in range(3):
+                colorList.append(0)
+        na = np.array(colorList).reshape((n,n,3))
+        print(len(na))
+        result = Image.fromarray(na.astype(np.uint8))
+        result.save("result.png")    
+fileName="encodescript.txt"
+password = encoder(fileName)
+password.encode(12)
+
